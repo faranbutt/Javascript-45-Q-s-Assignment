20c17e0fb60d635ceeba81e8551366231a63ffef
Faran Butt <66089079+faranbutt@users.noreply.github.com>
2022-10-02 23:04:20 +0500

    Add files via upload

---
+++ Exercise 10/Exercise10b.js
@@ -0,0 +1,8 @@
+//Declaration of variable famous_person with value Albert Einstein
+var famous_person="Albet Einstein";
+/*
+Declaration of variable message with a famous quote
+Then printing out the quote and author.
+*/
+var message="\"Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.\""
+console.log(famous_person+" "+"once said,"+" "+message);
