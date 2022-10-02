070a4c716f78f07e72a86bd85b505a1cfb5aadd9
Faran Butt <66089079+faranbutt@users.noreply.github.com>
2022-10-02 23:03:41 +0500

    Delete Exercise10 directory

--- Exercise10/Exercise10a.js
+++
@@ -1,5 +0,0 @@
-//Intializing a variable favouriteNumber with value 15
-var favouriteNumber=15;
-
-//printing out the faviourite number using conole.log() funtion.
-console.log("My favourite number is "+" "+favouriteNumber+" "+"because its my birth date number");
